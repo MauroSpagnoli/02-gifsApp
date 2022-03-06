747023fd279bf3c5daa3da8df6a78918473d891c
MauroSpagnoli <maurospagnoliherrera@hotmail.com>
2022-03-06 13:44:09 +0100

    Final project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/sidebar/shared.module';
+import { GifsModule } from '../gifs/gifs.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,SharedModule,GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
